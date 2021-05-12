4db3466cdd71a4b1c5230b104523655996e46b93
talosiot-will <68340168+talosiot-will@users.noreply.github.com>
2021-05-12 15:00:11 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,4 @@ function main() {
     echo "RUNNING!!"
 }
 
-
-function get_keys() {
-
-    which jq || (echo "Must have jq installed: sudo apt install -y jq" && false)
-    mkdir -p __deploykeys
-    for key in $(shell grep '@git+ssh' settings.ini | sed -n -e 's/^.*\(@git+ssh:\/\/\)//p') ; do \
-        fname=__deploykeys/$$( basename $$key ).key && echo $$fname ; \
-        aws secretsmanager get-secret-value --secret-id $$key | jq '.SecretString' | tr -d '"' | sed 's/\\n/\n/g' > $$fname && \
-        chmod 600 $$fname ; \
-    done
-}
-
 main
