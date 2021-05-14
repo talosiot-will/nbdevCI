06b03af9f5b395b28877b68fd2a3cf918a2370bc
Will Gathright <wgathright@talosiot.com>
2021-05-14 14:23:28 +0000

    Added better CLI options to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ ADD install_requirements /install_requirements
 RUN ["chmod", "+x", "/install_requirements"]
 RUN /install_requirements
 
-ADD ci_funcs /ci_funcs
 ADD entrypoint.sh /entrypoint.sh
 RUN ["chmod", "+x", "/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,69 @@ set -e
 DEPLOYKEYS="${INPUT_DEPLOYKEYS:-__deploykeys}"
 nbdev_test_nbs_args="${INPUT_NBDEV_TEST_NBS_ARGS}"
 
-source ./ci_funcs
-main
+function main () {
+    get_keys
+    install_library
+    nbdev_read_nbs
+    check_for_clean_nbs
+    check_for_library_nb_diff
+    nbdev_test_nbs $nbdev_test_nbs_args
+}
+
+function get_keys () {
+    which jq || (echo "Must have jq installed: sudo apt install -y jq" && false)
+    mkdir -p $DEPLOYKEYS
+    for key in $(grep '@git+ssh' settings.ini | sed -n -e 's/^.*\(@git+ssh:\/\/\)//p') ; do
+        fname=__deploykeys/$( basename $key ).key && echo $fname ;
+        aws secretsmanager get-secret-value --secret-id $key | jq '.SecretString' | tr -d '"' | sed 's/\\n/\n/g' > $fname
+        chmod 600 $fname ;
+    done
+}
+
+function install_library () {
+    if ls $DEPLOYKEYS/*.key 1> /dev/null 2>&1;
+        then
+            mkdir -p ~/.ssh
+            eval `ssh-agent`
+            ssh-add $DEPLOYKEYS/*.key
+            ssh -o StrictHostKeyChecking=no -T git@github.com || true
+            #ssh into github with no stricthostkeychecking adds github.com to the known hosts
+            #but...
+            #ssh into github fails with an error code because github kicks you out
+        fi
+        pip install .
+}
+
+function check_for_clean_nbs () {
+    echo "Check we are starting with clean git checkout"
+    git status
+    if [ -n "$(git status -uno -s)" ]; then echo "git status is not clean"; false; fi
+    echo "Trying to strip out notebooks"
+    nbdev_clean_nbs
+    echo "Check that strip out was unnecessary"
+    git status -s # display the status to see which nbs need cleaning up
+    if [ -n "$(git status -uno -s)" ]; then echo -e "!!! Detected unstripped out notebooks\n!!!Remember to run nbdev_install_git_hooks"; false; fi
+}
+
+function check_for_library_nb_diff () {
+            if [ -n "$(nbdev_diff_nbs)" ]; then echo -e "!!! Detected difference between the notebooks and the library"; echo "$(nbdev_diff_nbs)"; false; fi
+}
+
+
+
+help() {
+    echo "Run CI steps for an nbdev project"
+    echo "-f Run an individual function"
+    false
+}
+
+FUNC="main"
+while getopts ":f:" arg; do
+    case $arg in
+        f) FUNC=$OPTARG;;
+        *) help ;;
+    esac
+done
+
+
+$FUNC
